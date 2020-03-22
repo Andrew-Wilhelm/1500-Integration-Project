e48346cc7ca80627445a5621ace91e88a1b94bef
Andrew-Wilhelm <62119560+Andrew-Wilhelm@users.noreply.github.com>
2020-03-21 20:20:34 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -4,46 +4,107 @@
 #dammage counter
 #combo counter
 
-print("Would you like to play a game? \nType yes or no")
-intro_answer = input()
-if intro_answer == "yes":
-    print("Welcome to Duck Hunt")
-else:
-    print("Welcome to Duck Hunt Any Way!"*10)
+import math
+import random
 
-print("Do you want to shoot duck 1 2 3 or 4?")
-duck = int(input())
-score = 0
-if duck == 1:
-    print("good job you got 500 points")
-    score += (500)
-elif duck == 2:
-    print("good job you got 1000 points")
-    score += (1000)
-elif duck == 3:
-    print("good job you got 1000 points")
-    score += (5000)
-elif duck == 4:
-    print("good job you got 1000 points")
-    score -= (3000)
-else:
-    print("That is not a duck you silly Goose")
+def introduction():
+    intro_answer = input("Would you like to play a game? \nType yes or no? ")
+    if intro_answer == "yes":
+        print("Welcome to Duck Hunt")
+    else:
+        print("Welcome to Duck Hunt Any Way!")
 
+def duck_shooting():
+    duck = input("Do you want to shoot duck 1 2 3 or 4?")
+    score = 0
+    if duck == 1:
+        if chance_hit() == 1:
+            print("good job you got 500 points")
+            score += (500)
+        else:
+            print("Dang, you missed")
+    elif duck == 2:
+        if chance_hit() == 1:
+            print("good job you got 1000 points")
+            score += (1000)
+        else:
+            print("Dang, you missed")
+    elif duck == 3:
+        
+        if chance_hit() == 1:
+            print("good job you got 1000 points")
+            score += (5000)
+        else:
+            print("Dang, you missed")
+    elif duck == 4:
+        if chance_hit() == 1:
+            print("good job you got 1000 points")
+            score += (3000)
+        else:
+            print("Dang, you missed")
+    else:
+        print("That is not a duck you silly Goose")
+    print("Score:"+("{:08d}".format(score)))
 
-print("Score:"+("{:08d}".format(score)))
+#Chance of hitting the duck
+#level_difficulty will lower as it gets harder
+def chance_hit():
+    rand_num = random.randint(1,14)
+    if rand_num >= level_difficulty():
+        hit = 1
+    else:
+        hit = 0
+    return hit
+
+#sets the number of ducks to apear in a round and how strong they will be
+def number_of_ducks():
+    num_ducks = random.randint(1,30)
+def duck_power():
+    number_of_ducks() 
+    for index in range (1,num_ducks):
+        level_difficulty()
+        power_lvl = lvl_diff
+
+        
+def level_difficulty():
+    lvl_diff = random.randint(1,5)
+    return lvl_diff
+
+def shop():
+    if score < item:
+        purchased = 0
+    else:
+        purchased = 1
+    
+def start():
+    introduction()
+    
+    duck_shooting()
+    
+    
+start()
 
 #use of required numerical operators
-print("pick a number that you want to be put to the power of 7, divided by 6 and added to 750")
-num = int(input())
+num = int(input("pick a number that you want to be put to the power of 7, divided by 6 and added to 750"))
 num = num ** 7 / 6 + 750
 print("result:",num)
-print("If you want just the whole number type 1 \nIf you want just the remainder then type 2")
-sqr_num = int(input())
+sqr_num = int(input("If you want just the whole number type 1 \nIf you want just the remainder then type 2"))
 if(sqr_num == 1):
     print("Heres the Whole side:" ,num // 1)
 else:
     print("Heres the remainder:", num % 1)
-    
+
+#more needed things
+def get_quadratic(a,b):
+    s = a**2 + b**2
+    return s
+
+# needed while function?
+s = 0
+q = int(input("try and find a number that when squared and addde to 6 squared will return 40: "))
+while s != 40:
+    get_quadratic(q,6)
+
 #duck Points
 #duck1 = 500
 #duck2 = 1000
@@ -72,4 +133,3 @@ else:
 
 #print("Score:"+("{:08d}".format(score))) #so the score ends in the proper format
 
-print("Thats all there is, no more to do")
